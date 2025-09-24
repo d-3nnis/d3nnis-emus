d9fab5a0f320e0c9658cc4f3045be85a0956506c
Dennis Vidovic <dennisvidovic@gmail.com>
2025-09-23 17:58:50 -0700

    Broken

--- emu/emus/chip8/chip8.cpp
+++ emu/emus/chip8/chip8.cpp
@@ -1,15 +1,7 @@
 #include "chip8.hpp"
-
-// static SDL_Window *window = NULL;
-// static SDL_Renderer *renderer = NULL;
+#include "SDLDisplay.hpp"
 
 void initDisplay(void) {
-    SDL_Log("hgello");
-    // SDL_Init(SDL_INIT_VIDEO);
-    // SDL_WindowPtr win(SDL_CreateWindow("Test", SDL_WINDOWPOS_CENTERED,
-                                       // SDL_WINDOWPOS_CENTERED, DISPLAY_WIDTH, DISPLAY_HEIGHT, 0));
-    // SDL_Delay(2000); // Show for 2 seconds
-    // SDL_Quit();
 }
 
 void startChip8Emu(void) { initDisplay(); }
--- emu/emus/chip8/chip8.hpp
+++ emu/emus/chip8/chip8.hpp
@@ -1,4 +1,3 @@
-#include <SDL3/SDL.h>
 #include <cstdint>
 
 static constexpr int MEMORY_SIZE = 4096;
@@ -6,6 +5,5 @@ static constexpr uint8_t MEMORY[MEMORY_SIZE] = {};
 
 static constexpr int DISPLAY_WIDTH = 64;
 static constexpr int DISPLAY_HEIGHT = 32;
-
 
 void startChip8Emu(void);
